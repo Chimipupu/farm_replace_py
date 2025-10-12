d55e4a50d1897dcc63c782819d6c015b7c5acdd2
Chimipupu <chiminyanyan@gmail.com>
2025-10-12 23:44:02 +0900

    序盤の関数定義解放段階の実装

---
+++ farm_replace.py
@@ -0,0 +1,29 @@
+# 木作成
+def plant_Carrot():
+    if can_harvest():
+        harvest() # 収穫
+        plant(Entities.Bush)
+        till() # 土を耕す
+        plant(Entities.Carrot) # 人参を埋める
+        if get_water() < 1:
+            use_item(Items.Water) # 水やり
+    else :
+        move(East)
+
+# 茂み作成
+def plant_Bush():
+    move(North) # 北に移動
+    if can_harvest():
+        harvest() # 収穫
+        plant(Entities.Bush)
+    else :
+        move(East)
+
+while True:
+    move(North) # 北に移動
+    # plant_Bush()
+    plant_Carrot()
+
+    # move(East)
+    # plant_Bush()
+    plant_Carrot()
