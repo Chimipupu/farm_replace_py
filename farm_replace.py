dba6286e42ec721670a2cc457a3d6772a08b4bdb
Chimipupu <chiminyanyan@gmail.com>
2025-10-13 02:01:21 +0900

    木の植え方を修正

--- farm_replace.py
+++ farm_replace.py
@@ -1,26 +1,30 @@
 def farm_SpeedUp():
     if get_water() < 1:
-        use_item(Items.Water) # 水やり
+        if num_items(Items.Water) > 0:
+            use_item(Items.Water) # 水やり
     else :
-        use_item(Items.Fertilizer)
+        if num_items(Items.Fertilizer) > 0:
+            use_item(Items.Fertilizer)  # 肥料
 
 # にんじん作成
 def plant_Carrot():
-    if can_harvest():
-        harvest() # 収穫
-        till() # 土を耕す
-        plant(Entities.Carrot) # 人参を埋める
-        farm_SpeedUp()
-
-# かぼちゃ作成
-# NOTE: かぼちゃは20%の確率で枯れるので対策でtill()
-def plant_Pumpkin():
-    if can_harvest():
-        harvest() # 収穫
+    if get_ground_type() != Grounds.Soil:
         till() # 土を耕す
     else :
-        plant(Entities.Pumpkin)
-        farm_SpeedUp()
+        if can_harvest():
+            harvest() # 収穫
+            plant(Entities.Carrot) # 人参を埋める
+            farm_SpeedUp()
+
+# かぼちゃ作成
+def plant_Pumpkin():
+    if get_ground_type() != Grounds.Soil:
+        till() # 土を耕す
+    else :
+        if can_harvest():
+            harvest() # 収穫
+            plant(Entities.Pumpkin) # かぼちゃを植える
+            farm_SpeedUp()
 
 # 茂み作成
 def plant_Bush():
@@ -29,6 +33,7 @@ def plant_Bush():
         plant(Entities.Bush)
 
 # 木作成
+# NOTE: 木は四方に木で囲うと成長側が1/16になる
 def plant_Tree():
     if can_harvest():
         harvest() # 収穫
@@ -42,14 +47,18 @@ def plant_Hay():
         till() # 土を耕す
 
 while True:
-    # clear()
+    clear()
 
     i = 0
-    for i in range(8):
+    for i in range(12): # 農地面積 12x12
         # plant_Hay()         # 草
         # plant_Bush()        # 茂
-        # plant_Tree()        # 木
-        plant_Carrot()      # 人参
+
+        # NOTE: 木は四方に木で囲うと成長側が1/16になる
+        if(i % 2 == 0):
+            plant_Tree()        # 木
+
+        # plant_Carrot()      # 人参
         # plant_Pumpkin()     # かぼちゃ
         move(North) # 北に移動
 
