70e69b134266fdae7005cb3880c699963d664632
Chimipupu <chiminyanyan@gmail.com>
2025-10-13 00:11:18 +0900

    木をアンロックしたので木を農作業

--- farm_replace.py
+++ farm_replace.py
@@ -6,38 +6,48 @@ def plant_Carrot():
         plant(Entities.Carrot) # 人参を埋める
         if get_water() < 1:
             use_item(Items.Water) # 水やり
+        else :
+            use_item(Items.Fertilizer)
+
+# かぼちゃ作成
+def plant_Pumpkin():
+    if can_harvest():
+        harvest() # 収穫
+        till() # 土を耕す
+        plant(Entities.Pumpkin)
+        if get_water() < 1:
+            use_item(Items.Water) # 水やり
+        else :
+            use_item(Items.Fertilizer)
     else :
-        move(East)
+        till() # 土を耕す
 
 # 茂み作成
 def plant_Bush():
-    move(North) # 北に移動
     if can_harvest():
         harvest() # 収穫
         plant(Entities.Bush)
     else :
-        move(East)
+        till()
 
-# 茂み作成
-def plant_Pumpkin():
+# 木作成
+def plant_Tree():
     if can_harvest():
         harvest() # 収穫
-        # till() # 土を耕す
-        plant(Entities.Pumpkin)
+        till() # 土を耕す
+        plant(Entities.Tree)
         if get_water() < 1:
             use_item(Items.Water) # 水やり
-    else :
-        move(East)
+        else :
+            use_item(Items.Fertilizer)
 
-# 茂み作成
 while True:
-    move(North) # 北に移動
-    # plant_Bush()
-    # plant_Carrot()
-    plant_Pumpkin()
+    i = 0
+    for i in range(7):
+        move(North) # 北に移動
+        # plant_Bush()
+        plant_Tree()
+        # plant_Carrot()
+        # plant_Pumpkin()
 
-    # move(East)
-    move(South)
-    plant_Bush()
-    # plant_Carrot()
-    plant_Pumpkin()
+    move(East)
