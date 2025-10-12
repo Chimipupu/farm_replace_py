fc29bec15758dd59283c488af18b37fb1dc49bf7
Chimipupu <chiminyanyan@gmail.com>
2025-10-12 23:49:19 +0900

    かぼちゃ解放段階の実装

--- farm_replace.py
+++ farm_replace.py
@@ -1,8 +1,7 @@
-# 木作成
+# にんじん作成
 def plant_Carrot():
     if can_harvest():
         harvest() # 収穫
-        plant(Entities.Bush)
         till() # 土を耕す
         plant(Entities.Carrot) # 人参を埋める
         if get_water() < 1:
@@ -19,11 +18,26 @@ def plant_Bush():
     else :
         move(East)
 
+# 茂み作成
+def plant_Pumpkin():
+    if can_harvest():
+        harvest() # 収穫
+        # till() # 土を耕す
+        plant(Entities.Pumpkin)
+        if get_water() < 1:
+            use_item(Items.Water) # 水やり
+    else :
+        move(East)
+
+# 茂み作成
 while True:
     move(North) # 北に移動
     # plant_Bush()
-    plant_Carrot()
+    # plant_Carrot()
+    plant_Pumpkin()
 
     # move(East)
-    # plant_Bush()
-    plant_Carrot()
+    move(South)
+    plant_Bush()
+    # plant_Carrot()
+    plant_Pumpkin()
